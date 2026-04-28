498a4c9dbc02e85d6a097ff5143ebb059aca2fd8
fzhang15 <34219442+fzhang15@users.noreply.github.com>
2026-04-27 23:26:21 -0700

    Update solution for binary-focal-loss

--- binary-focal-loss/binary-focal-loss.py
+++ binary-focal-loss/binary-focal-loss.py
@@ -5,6 +5,8 @@ def binary_focal_loss(predictions, targets, alpha, gamma):
     Compute the mean binary focal loss.
     """
     # Write code here
-    p_t = np.array([p if t == 1 else 1 - p for t, p in zip(targets, predictions)])
+    preds = np.array(predictions)
+    targs = np.array(targets)
+    p_t = np.where(targs == 1, preds, 1 - preds)
     fl = -np.mean(alpha * (1 - p_t) ** gamma * np.log(p_t))
     return fl
