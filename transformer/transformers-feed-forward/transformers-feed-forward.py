59e845b344237cd0a8a876a46fbfdf947c2ccbd9
fzhang15 <34219442+fzhang15@users.noreply.github.com>
2026-05-10 20:20:29 -0700

    Add solution for transformers-feed-forward

---
+++ transformer/transformers-feed-forward/transformers-feed-forward.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def feed_forward(x: np.ndarray, W1: np.ndarray, b1: np.ndarray,
+                 W2: np.ndarray, b2: np.ndarray) -> np.ndarray:
+    """
+    Apply position-wise feed-forward network.
+    """
+    # Your code here
+    h = np.matmul(x, W1) + b1
+    h = np.maximum(h, 0)
+    out = np.matmul(h, W2) + b2
+
+    return out
